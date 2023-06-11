d33427b64ba230b4f495836d0f99332a7c89c4ca
ihsanuddienullah <ihsanuddienullah@gmail.com>
2023-06-11 09:59:37 +0700

    function

--- function.js
+++ function.js
@@ -1,1 +1,22 @@
 // Functions
+function greet(name) {
+  console.log("Hello there " + name);
+}
+
+greet("Hafiq");
+greet("Ihsan");
+
+function add(a, b) {
+  return a + b;
+}
+
+let x = add(5, 2);
+console.log(x);
+
+function perkalian(a, b) {
+  console.log(a);
+  return a * b;
+}
+
+let y = perkalian(10, 5);
+console.log(y);
