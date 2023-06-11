73bebf17cc09d2b0bbbee9c3d68f886b7acee100
ihsanuddienullah <ihsanuddienullah@gmail.com>
2023-06-11 10:03:56 +0700

    function update

--- function.js
+++ function.js
@@ -1,18 +1,25 @@
 // Functions
-function greet(name) {
+
+// Fungsi greet
+function greet(name) { 
   console.log("Hello there " + name);
 }
 
 greet("Hafiq");
 greet("Ihsan");
 
+// Fungsi penjumlahan
+let hasilTambah;
 function add(a, b) {
+  hasilTambah = a + b;
   return a + b;
 }
 
 let x = add(5, 2);
 console.log(x);
+console.log(hasilTambah);
 
+// Fungsi perkalian
 function perkalian(a, b) {
   console.log(a);
   return a * b;
